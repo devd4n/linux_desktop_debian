2d6d8376476a2a55f78c0cb058b8d0d17fb60b1b
devd4n <18461526+devd4n@users.noreply.github.com>
2023-11-12 16:46:10 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,9 @@ setxkbmap -print -verbose 10
 echo "GTK_IM_MODULE=ibus" >> /etc/environment
 echo "QT_IM_MODULE=ibus" >> /etc/environment
 echo "XMODIFIERS=@im=ibus" >> /etc/environment
+
+
+#download this script to the bin folder
+sudo wget -qO /usr/bin/apt-add-repo https://raw.githubusercontent.com/devd4n/apt-add-repo/main/apt-add-repo.sh
+#make it executable
+sudo chmod +x /usr/bin/apt-add-repo
