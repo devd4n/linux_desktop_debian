1ed54d314749752f156851c99fc8fdb1a63c699f
devd4n <18461526+devd4n@users.noreply.github.com>
2023-11-12 16:42:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,14 @@
+#!bin/bash
 
+sudo apt-get install kde-standard
+
+sudo /usr/sbin/dpkg-reconfigure keyboard-configuration
+# => if that not works after reboot
+
+setxkbmap -print -verbose 10
+# => check status
+
+# Use ibus as input Module:
+echo "GTK_IM_MODULE=ibus" >> /etc/environment
+echo "QT_IM_MODULE=ibus" >> /etc/environment
+echo "XMODIFIERS=@im=ibus" >> /etc/environment
