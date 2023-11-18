d7d7772eb83538210afe2d46bfa3a36b5925f853
devd4n <18461526+devd4n@users.noreply.github.com>
2023-11-18 19:47:36 +0100

    Update install_signal.sh

--- software/install_signal.sh
+++ software/install_signal.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-add-repo signal-desktop https://updates.signal.org/desktop/apt/keys.asc https://updates.signal.org/desktop/apt xenial main
-apt-get update
-apt-get upgrade
+apt-get update -y
+apt-get install signal-desktop -y
