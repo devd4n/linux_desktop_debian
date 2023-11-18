0a104be9117a2b8f443a172fd720b883e6cede86
devd4n <18461526+devd4n@users.noreply.github.com>
2023-11-18 20:38:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,9 +19,11 @@ sudo wget -qO /usr/bin/apt-add-repo https://raw.githubusercontent.com/devd4n/apt
 #make it executable
 sudo chmod +x /usr/bin/apt-add-repo
 
+
 # install over PackageMgr
 xargs sudo apt-get -y install < software/apt_packages_basic.txt
 
+/bin/bash software/install_wireshark.sh
 /bin/bash software/install_signal.sh
 /bin/bash software/install_sublime_text.sh
 # Virtualbox script must be upgraded to work properly:
