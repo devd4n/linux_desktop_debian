f5494888fb8e51c5402b46f9dcb7aa12d9c22aeb
devd4n <18461526+devd4n@users.noreply.github.com>
2023-11-18 19:47:02 +0100

    Update install_sublime_text.sh

--- software/install_sublime_text.sh
+++ software/install_sublime_text.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 apt-add-repo sublime-text https://download.sublimetext.com/ https://download.sublimetext.com/sublimehq-pub.gpg apt/stable/
+apt-get update -y
+apt-get install sublime-text sublime-merge -y
